15c1a900ef05eb8051bd3383f18b07c400aad5f6
Vlad <vlad.petrovskiy00@mail.ru>
2024-12-07 22:07:06 +0300

    some new code in EnterPage

--- src/js/components/EnterPage.js
+++ src/js/components/EnterPage.js
@@ -1,30 +1,47 @@
-
 import React from "react";
 import { useState } from 'react';
-import { regUser } from "../services/userService";
+import { _NotFoundUserMess, regUser } from "../services/userService";
+import { getUser } from "../services/userService";
+import userComp from "./UserComponent";
+import MainPage from "./MainPage";
+import ListItems from "./ListItems";
+import { backToMain } from "../services/mainPageService";
+
 class EnterPage extends React.Component{
     constructor(props){
         super(props)
-        this.state={data: {}}
+        this.state={data: {}, operMessage: ""} 
     }
     render()
-    {return(
-        <div id="enterDiv">
-            <br></br>
-            <form id="entForm" method="post">
-                <input type="text" id="login"/>
-                <label for="login">Логин</label><br/>
-                <br/><br/>
-                <input type="text" id="password"/>
-                <label for="password">Пароль</label><br/>
-                <button type="button" onClick={()=>
-                {
-                    
-                }
-                }>Войти</button>
-            </form>
-        </div>
-    );}
+    {
+        switch(this.state.operStatus){
+            case "2":
+                return(<MainPage element={<ListItems/>}/>);
+            case _NotFoundUserMess:
+                alert(this.state.operMessage);
+                break
+            default:
+                return(
+                <div id="enterDiv">
+                    <br></br>
+                    <form id="entForm" method="post">
+                        <input type="text" id="login"/>
+                        <label for="login">Логин</label><br/>
+                        <br/><br/>
+                        <input type="text" id="password"/>
+                        <label for="password">Пароль</label><br/>
+                        <button type="button" onClick={()=>
+                        {
+                            let login = document.getElementById("login").value;
+                            let password = document.getElementById("password").value;
+                            getUser(login, password, this)
+                        }
+                        }>Войти</button>
+                    </form>
+                </div>
+            );
+        }
+    }
 }
 
 export default EnterPage
--- src/js/services/userService.js
+++ src/js/services/userService.js
@@ -3,7 +3,10 @@ import { root } from "../../index.js";
 import MainPage from "../components/MainPage.js";
 import { useState } from "react";
 
-const URL = "http://localhost:8080/users"
+const URL = "http://localhost:8080/users";
+export const _NotFoundUserMess = "Такого пользователя не существует";
+export const _NotIdentifiedUserMess = "Неправильный пароль";
+export const _UserAreRegMess = "Пользователь уже зарегестрирован";
 
 export async function regUser(user) {
     await axios.post(URL + "/reg", user, {
@@ -13,13 +16,25 @@ export async function regUser(user) {
 )
 }
 
-export async function takeUser(login, user){
-    await axios.get(URL + "/" + login).then(function(response){
-            user.setState({
-                data: response.data
-            })
-
-            root.render(<MainPage/>)
+export async function getUser(login, password, enter){ //getUser if response.status
+    await axios.get(URL + "/signin" + "/" + login + "/" + password).then(function(response){
+        console.log(response)
+        switch(response.data.message){
+            case "2":
+                enter.setState({
+                    data: response.data
+                });
+                break
+            case _NotFoundUserMess:
+                enter.setState({
+                    operMessage: response.message
+                })
+                break
+            default:
+                enter.setState({
+                    operMessage: "Неизвестная ошибка"
+                })
+                break
         }
-    )
+    });
 }
